8b4f23e7ca07079ecd1aebdd99f507009952ade0
Lucas <lucasangelloti@gmail.com>
2025-10-02 08:59:12 -0300

    quinto commit
    Cadastrei o método GET, e testei as requisições POST e GET no Postman, retornando 200 OK

--- src/main/java/com/vetexpert/prontuario_api/controllers/TutorController.java
+++ src/main/java/com/vetexpert/prontuario_api/controllers/TutorController.java
@@ -4,10 +4,9 @@ package com.vetexpert.prontuario_api.controllers;
 import com.vetexpert.prontuario_api.model.Tutor;
 import com.vetexpert.prontuario_api.repositories.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController //essa anotação indica que é uma classe Controller REST
 @RequestMapping("/api/tutores") //url base para os métodos presentes na classe
@@ -20,4 +19,9 @@ public class TutorController {
     public void cadastrar(@RequestBody Tutor dados){
         repository.save(dados);
     }//requestbody traduz o JSON da requisição e organiza o objeto de acordo com os setters
+
+    @GetMapping//faz com que as requisições GET caiam nesse método
+    public List<Tutor> listar(){
+        return repository.findAll();//usa o repositório para buscar todos os registros
+    }
 }
