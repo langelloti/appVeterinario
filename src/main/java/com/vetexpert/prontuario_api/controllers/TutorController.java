a5717ceba131efa3ec3a127ae83af5770b621232
Lucas <lucasangelloti@gmail.com>
2025-10-02 09:28:00 -0300

    sexto commit
    Adicionei os métodos DELETE e PUT, com comentários para fixar entendimento, ambos testados no postman e recebi 200 OK, próximo passo irei criar os controllers de veterinario e testar

--- src/main/java/com/vetexpert/prontuario_api/controllers/TutorController.java
+++ src/main/java/com/vetexpert/prontuario_api/controllers/TutorController.java
@@ -18,10 +18,21 @@ public class TutorController {
     @PostMapping//faz com que as requisições POST caiam nesse método
     public void cadastrar(@RequestBody Tutor dados){
         repository.save(dados);
-    }//requestbody traduz o JSON da requisição e organiza o objeto de acordo com os setters
+    }//requestbody traduz o JSON da requisição e organiza o objeto conforme os setters
 
     @GetMapping//faz com que as requisições GET caiam nesse método
     public List<Tutor> listar(){
         return repository.findAll();//usa o repositório para buscar todos os registros
+    }
+
+    @PutMapping("/{id}")// esse método tendo o "/{id}" adiciona no final do caminho http o direcionamento, garantindo que o PUT seja aplicado apenas ao tutor de id correspondente
+    public void atualizar(@PathVariable Long id, @RequestBody Tutor dados){//o path variable identifica qual tutor será atualizado e o request body fornece os novos dados para atualização
+        dados.setId(id);//garante que o id do objeto seja o mesmo da url
+        repository.save(dados);//salva os dados atualizados no repositório
+    }
+
+    @DeleteMapping("/{id}")// mesma ideia do id de put mapping, mas dessa vez garantindo o método DELETE ao usuário de id correspondente
+    public void deleteById(@PathVariable Long id){//como não há uma atualização e sim uma exclusão, ele apenas exclui tudo que encontra no id
+        repository.deleteById(id); //aplica o método delete ao id correspondente no repositório
     }
 }
